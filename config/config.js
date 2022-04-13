02e1ec5a43e7da5b897e709fd92b740318429a10
Bárbara Zamperete <bahzamp25@gmail.com>
2022-04-13 15:34:38 -0400

    hh

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   production: {
     use_env_variable: process.env.HEROKU_POSTGRESQL_GRAY_URL,
-    dialect: postgres,
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
